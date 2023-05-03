84a4e261c08b0215e41b2ce233e1fad9dfe340a3
Akhand Vaibhav Singh <72245505+akhandvaibhav17@users.noreply.github.com>
2023-05-03 09:25:26 +0530

    Update connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const mongoos = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/masterMarauders", {}).then(() => {
+mongoose.connect("mongodb+srv://akhandvaibhav17:0e2hQAktQ2i68bSP@cluster0.uvfrofw.mongodb.net/?retryWrites=true&w=majority", {}).then(() => {
     console.log(`connection successful`);
 }).catch((e) => {
     console.log(`no connection`);
